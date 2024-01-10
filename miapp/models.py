48e1738548213cec9630e7615a0d00d70d16b9f4
Sonicrojas321 <sonic_64ds@hotmail.com>
2024-01-10 00:27:06 -0600

    Agregar Models

--- miapp/models.py
+++ miapp/models.py
@@ -2,3 +2,10 @@ from django.db import models
 #class Servidor(models.Model):
  #   nombre = models.CharField(max_length=40)
 # Create your models here.
+
+class Bots(models.Model):
+    servidor = models.CharField(max_length=50)
+    contra = models.CharField(max_lenght=256)
+
+#class Reportes(Models.Model):
+    #hora = models.
