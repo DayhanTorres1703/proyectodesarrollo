54280d8b3c7a6d875d7646f016d65f9ef3e139f6
Sonicrojas321 <sonic_64ds@hotmail.com>
2024-01-11 18:09:24 -0600

    Agregado de Llaves foraneas

--- miapp/models.py
+++ miapp/models.py
@@ -7,11 +7,6 @@ class Bots(models.Model):
     servidor = models.CharField(max_length=50)
     contra = models.CharField(max_length=256)
 
-class Reportes(models.Model):
-    hora = models.DateTimeField(auto_now_add=True)
-    estado = models.CharField(max_length=20)
-    nombre = models.CharField(max_length=50)
-
 class Servidor(models.Model):
     nombre = models.CharField(max_length=255)
     ip = models.GenericIPAddressField()
@@ -19,13 +14,19 @@ class Servidor(models.Model):
     contrasena_bot = models.CharField(max_length=255)
 
 class RegistroRespaldo(models.Model):
-    servidor_origen = models.CharField(max_length=255)
+    ip_origen = models.GenericIPAddressField(max_length=255)
     directorio_origen = models.CharField(max_length=255)
-    servidor_destino = models.CharField(max_length=255)
+    ip_destino = models.GenericIPAddressField(max_length=255)
     directorio_destino = models.CharField(max_length=255)
     periodicidad = models.CharField(max_length=255)
     fecha_respaldo = models.DateTimeField(auto_now_add=True)
+    servidor_origen = models.ForeignKey(Servidor, on_delete=models.CASCADE)
 
+class Reportes(models.Model):
+    hora = models.DateTimeField(auto_now_add=True)
+    estado = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=50)
+    respaldo_origen = models.ForeignKey(RegistroRespaldo, on_delete=models.CASCADE)
 
 class Prueba(models.Model):
     nombre = models.CharField(max_length=50)
