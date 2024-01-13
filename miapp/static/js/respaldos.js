b0a2e7acf5cb59bea5a38d969757acf40ee9d31b
Sonicrojas321 <sonic_64ds@hotmail.com>
2024-01-13 01:53:10 -0600

    Funcionamiento de sockets

--- miapp/static/js/respaldos.js
+++ miapp/static/js/respaldos.js
@@ -1,12 +1,19 @@
 $(function(){
-    $("#enviarRespaldo").click(function(){
-        $.post("/respaldar_servidor/", {ipOrigen: $("#servidorOrigen").val(), ipDestino: $("#servidorDestino").val(), dirOrigen: $("#directorioOrigen").val(), dirDestino: $("#directorioDestino").val(), cron: $("#periodicidad").val()}),
+    $("#enviarRespaldo").click(function() {
+        var cron_unido = $("#minutos").val() + " " + $("#horas").val() + " " + $("#dias_mes").val() + " " + $("#meses").val() + " " + $("#dias_semana").val();
+        var cron_cadena = cron_unido.toString();
+        $.post( "/respaldar_servidor/", 
+        { ipOrigen: $("#servidorOrigen").val(), 
+        ipDestino: $("#servidorDestino").val(), 
+        dirOrigen: $("#directorioOrigen").val(), 
+        dirDestino: $("#directorioDestino").val(), 
+        cron: cron_cadena},
         function(data, status){
             if(status == "success"){
                 if(data.status != "OK"){
                     alert("No se lograron mandar los datos");
                 }
             }
-        }
+        });
     });
 });
